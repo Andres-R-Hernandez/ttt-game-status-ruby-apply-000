a518a24df2c0bb104c85b040a46f842407fa2ea0
Andres Hernandez <andres.hernandez@utexas.edu>
2020-09-26 16:59:32 +0000

    Automatically backed up by Learn

--- lib/game_status.rb
+++ lib/game_status.rb
@@ -17,17 +17,23 @@ WIN_COMBINATIONS =[
 
 #won? Method
 def won?(board)
-  WIN_COMBINATIONS.each do |combo|
-    if (board[combo[0]] == "X" && board[combo[1]] == "X" && board[combo[2]] == "X")
-      return combo
-    elsif (board[combo[0]] == "O" && board[combo[1]] == "O" && board[combo[2]] == "O")
-      return combo
-    elsif (board[combo[0]] == " " && board[combo[1]] == " " && board[combo[2]] == " ")
-      nil
-    else
-      nil
+
+  if board.all? {|i| i == " "}
+    false
+  elsif board.none? {|i| i == " "}
+    false
+  else
+    WIN_COMBINATIONS.each do |combo|
+      if (board[combo[0]] == "X" && board[combo[1]] == "X" && board[combo[2]] == "X")
+        return combo
+      elsif (board[combo[0]] == "O" && board[combo[1]] == "O" && board[combo[2]] == "O")
+        return combo
+      elsif (board[combo[0]] == " " && board[combo[1]] == " " && board[combo[2]] == " ")
+        nil
+      else
+        nil
+      end
     end
-  end
   !board.all? {|i| i == " "}
   !board.none? {|i| i == " "}
 end
