8d37e9d07f9f0db597c96f276f0cd4aa23905d82
Andres Hernandez <andres.hernandez@utexas.edu>
2020-09-26 16:21:58 +0000

    Automatically backed up by Learn

--- lib/game_status.rb
+++ lib/game_status.rb
@@ -19,6 +19,13 @@ WIN_COMBINATIONS =[
 
 #won? Method
 def won?(board)
+
+  if board.all? do |position|
+    position == " "
+    end
+    nil
+  end
+
   WIN_COMBINATIONS.each do |combo|
     if (board[combo[0]] == "X" && board[combo[1]] == "X" && board[combo[2]] == "X")
       return combo
