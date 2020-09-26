dd2f943308d6ee0f3eb56ccf22a2bc865cd60c7c
Andres Hernandez <andres.hernandez@utexas.edu>
2020-09-26 18:28:21 +0000

    Automatically backed up by Learn

--- lib/game_status.rb
+++ lib/game_status.rb
@@ -25,6 +25,8 @@ def won?(board)
         return combo
       elsif (board[combo[0]] == "O" && board[combo[1]] == "O" && board[combo[2]] == "O")
         return combo
+      else
+        nil
       end
     end
   end
